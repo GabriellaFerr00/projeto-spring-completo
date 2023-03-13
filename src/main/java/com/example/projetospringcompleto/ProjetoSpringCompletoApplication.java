8043ce58c492baa23fad2d90003fc209aa9572e9
Gabriella <gabriellalok3@gmail.com>
2023-03-13 03:26:38 -0300

    iniciando projeto spring boot

---
+++ src/main/java/com/example/projetospringcompleto/ProjetoSpringCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetospringcompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringCompletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetospringcompleto/ProjetoSpringCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetospringcompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
